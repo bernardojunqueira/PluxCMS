efa273c59a62f808aeb7a80682fd55cf4ff937f8
bjunqueira <bjunqueira@192.168.1.96>
2016-05-12 19:52:09 -0300

    development version 0.0.1

---
+++ src/main/java/br/com/plux/cms/config/SpringSecurityInitializer.java
@@ -0,0 +1,10 @@
+package br.com.plux.cms.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+    // Do nothing
+ 
+}
---
+++ src/main/java/br/com/plux/cms/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.plux.cms.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.jdbcAuthentication()
+				.dataSource(dataSource)
+				.withDefaultSchema()
+				.withUser("user").password("password").roles("USER");
+	}
+
+}
