c6cea71cd879c987fe94026647112082fcdfb146
Bernardo Junqueira <bjunqueira@me.com>
2016-05-19 20:14:16 -0300

    development version 0.0.8

--- src/main/java/br/com/plux/cms/repository/UserRepository.java
+++ src/main/java/br/com/plux/cms/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package br.com.plux.cms.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.plux.cms.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	User findByEmail(String email);
+	
+	@Query("from User u left join fetch u.roles where u.id=?1")
+    User findByIdWithRoles(Long id);
+	
+	@Query("from User u left join fetch u.roles")
+    List<User> findAllWithRoles();
 }
--- src/main/java/br/com/plux/cms/web/UserController.java
+++ src/main/java/br/com/plux/cms/web/UserController.java
@@ -8,11 +8,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.WebRequest;
 
 import br.com.plux.cms.model.User;
+import br.com.plux.cms.repository.RoleRepository;
 import br.com.plux.cms.repository.UserRepository;
 
 @Controller
@@ -23,20 +24,48 @@ public class UserController {
     private UserRepository userRepository;
 	
 	@Autowired
+	private RoleRepository roleRepository;
+
+	
+	@Autowired
     private PasswordEncoder passwordEncoder;
 	
 	@RequestMapping(value = {"", "/list"}, method = RequestMethod.GET)
-	public String showUsers (Model model) {
-		model.addAttribute("users", userRepository.findOne((long) 1));
+	public String list(Model model) {
+		model.addAttribute("users", userRepository.findAllWithRoles());
 		return "/users/index";
 	}
-
-	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public String showRegistrationForm(WebRequest request, Model model) {
-	    User user = new User();
-	    model.addAttribute("user", user);
-	    return "registerForm";   
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public String newUser(Model model) {
+		model.addAttribute("user", new User());
+		model.addAttribute("roles", roleRepository.findAll());
+		return "/users/userForm";
 	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String saveUser(@Valid User user, Model model, Errors errors) {
+		if (errors.hasErrors()) {
+			return "/users/userForm";
+	    }
+		System.out.println(user.getRoles());
+		//userRepository.save(user);
+		model.addAttribute("message", "Usuário adicionado com sucesso");
+		return "redirect:/users";
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String showUser (@PathVariable("id") Long id, Model model) {
+		model.addAttribute("user", userRepository.findOne(id));
+		return "/users/show";
+	}
+
+//	@RequestMapping(value = "/register", method = RequestMethod.GET)
+//	public String showRegistrationForm(WebRequest request, Model model) {
+//	    User user = new User();
+//	    model.addAttribute("user", user);
+//	    return "registerForm";   
+//	}
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String registerUserAccount(@Valid User user, Errors errors) {
